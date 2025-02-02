ed465f47ce340d31fc609e9392193c2cfb85111b
khacho138 <khachidzegiorgi138@gmail.com>
2025-02-02 18:29:36 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "დარწმუნებული ხარ?",
+    "კარგად დაფიქრდი??",
+    "უფრო კარგად?",
+    "სალომეეეე...",
+    "მეწყინება!",
+    "გავბრაზდები...",
+    "ძალიან გავბრაზდები...",
+    "ძალიან მეწყინება...",
+    "კარგი მეტს აღარ გკითხავ...",
+    "ვხუმრობ, უბრალოდ დააშირე კის! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
